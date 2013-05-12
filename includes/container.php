c9b0cf3465d64223671ced8963b4db93d052aae6
Mark Tertishniy <mtertishniy@gmail.com>
2013-05-12 16:30:07 +1100

    Change container file. Add new included libs.

--- includes/container.php
+++ includes/container.php
@@ -1,14 +1,17 @@
 <?php
    session_start();
 
-   require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/settings.php');
-   require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/constants.php');
-   require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/class.DataHandling.php');
-   require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/class.UserControl.php');
-   require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/smarty/libs/Smarty.class.php');
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/settings.php';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/constants.php';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/class.DataHandling.php';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/smarty/libs/Smarty.class.php';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/exception.inc';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/encryption.inc';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/cookie.inc';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/userDB.inc';
+   require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/reg_auth.inc';
 
-   $user   = new UserControl();
-   $data_h = new dataHandling();
+   $data_h = new DataHandling();
    $smarty = new TSmarty();
 
    $smarty->force_compile = true;
