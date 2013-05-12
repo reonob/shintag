2530687f76f0f5233da7114508c696d3b5ddb5da
Mark Tertishniy <mtertishniy@gmail.com>
2013-05-12 15:59:20 +1100

    Change mail class.

--- includes/class.Mail.php
+++ includes/class.Mail.php
@@ -2,6 +2,7 @@
 
    class Mail
    {
+      private $site = 'kolesa.su.com';
       private $isLocalhost = true;
 
       public function setIsLocalhost($isLocalhost)
@@ -9,15 +10,15 @@
           $this->isLocalhost = $isLocalhost;
       }
 
-      public function getUniqueSignature($email, $login)
+      public function getUniqueSignature($email, $login, $pass)
       {
          $parts = explode('@', $email);
-         return base64_encode(md5($login.$parts[0].md5($_SERVER['REMOTE_ADDR'].$parts[1])));
+         return base64_encode(md5(md5($parts[1].$login.$parts[0])).$pass);
       }
 
-      public function compareUniqueSignature($hash, $email, $login)
+      public function compareUniqueSignature($hash, $email, $login, $pass)
       {
-         return $hash == $this->getUniqueSignature($email, $login);
+         return $hash == $this->getUniqueSignature($email, $login, $pass);
       }
 
       private function saveToFile($email, $subject, $message)
@@ -25,57 +26,59 @@
          $filename = $email.'.txt';
          $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/mails/$filename", "w");
          $letter =
-                 "From: www.8cost.com\nSubject: $subject\nTo: $email\n\n$message";
+                 "From: $this->site\nSubject: $subject\nTo: $email\n\n$message";
          fwrite($file, $letter);
          fclose($file);
       }
 
-      public function sendActivationMail($email, $login)
+      public function sendActivationMail($email)
       {
+         $user = new UserDB($email);
+         $hash = $this->getUniqueSignature($email, $user->login, $user->password);
+
          $date = time();
          $subject = "Активация аккаунта";
-         $hash = $this->getUniqueSignature($email, $login);
-         $message = '8cost.com/includes/activation.php?type=activation&hash=' . $hash . '&email=' . $email;
+         $message = $this->site.'/includes/activation.php?type=activation&hash='.$hash.'&email='.$email;
          $header = 'Content-type:text/html; charset = utf-8';
+
          if ($this->isLocalhost) {
             $this->saveToFile($email, $subject, $message);
-         }
-         else {
+         } else {
             mail($email, $subject, $message, $header);
          }
       }
 
-      public function sendChangeMail($old_email, $new_email)
+      public function sendChangeMail($oldEmail, $newEmail)
       {
-         global $db_link;
+         $user = new UserDB($oldEmail);
+         $hash = $this->getUniqueSignature($newEmail, $user->login, $user->password);
+
          $date = time();
          $subject = "Подтвердение изменения e-mail.";
-         $login = $db_link->query('SELECT login FROM users WHERE email=?', [$old_email], true)[0]['login'];
-         $hash = $this->getUniqueSignature($new_email, $login);
-         $message = '8cost.com/includes/activation.php?type=change_email&hash=' . $hash
-                  . '&old_email=' . $old_email . '&new_email=' . $new_email;
+         $message = $this->site.'/includes/activation.php?type=change_email&hash='.$hash
+                   .'&old_email='.$oldEmail.'&new_email='.$newEmail;
          $header = 'Content-type:text/html; charset = utf-8';
+
          if ($this->isLocalhost) {
-            $this->saveToFile($new_email, $subject, $message);
-         }
-         else {
-            mail($new_email, $subject, $message, $header);
+            $this->saveToFile($newEmail, $subject, $message);
+         } else {
+            mail($newEmail, $subject, $message, $header);
          }
       }
 
-      public function sendForgottenPassMail($email, $login, $pass)
+      public function sendForgottenPassMail($email, $pass)
       {
+         $user = new UserDB($email);
+         $hash = $this->getUniqueSignature($email, $user->login, $user->password);
          $date = time();
          $subject = "Смена e-mail";
-         $hash = $this->getUniqueSignature($email, $login);
          $message =
-            'New password:' . "$pass\n" . '8cost.com/includes/activation.php?type=activation&subj=forgotten&hash='
-          . $hash . '&email=' . $email;
+            'New password:'."$pass\n".$this->site.'/includes/activation.php?type=activation&subj=forgotten&hash='
+           .$hash.'&email='.$email;
          $header = 'Content-type:text/html; charset = utf-8';
          if ($this->isLocalhost) {
             $this->saveToFile($email, $subject, $message);
-         }
-         else {
+         } else {
             mail($email, $subject, $message, $header);
          }
       }
